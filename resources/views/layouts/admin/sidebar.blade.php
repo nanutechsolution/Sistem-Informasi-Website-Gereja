c4a290bbc82654d7fee07e0ccd2d269affe352bc
nanu <ranusate19@gmail.com>
2025-08-03 01:04:05 +0800

    update

--- resources/views/layouts/admin/sidebar.blade.php
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -71,11 +71,11 @@
                             class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                             Manajemen Berita/Pengumuman</a>
                     </li>
-                    <li>
+                    {{-- <li>
                         <a href="{{ route('admin.pks-schedules.index') }}"
                             class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
                             Jadwal PKS </a>
-                    </li>
+                    </li> --}}
                     <li>
                         <a href="{{ route('admin.gallery-albums.index') }}"
                             class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
