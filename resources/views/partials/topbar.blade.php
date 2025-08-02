0896e54324dc35282269737bdb6cc281298a316b
nanu <ranusate19@gmail.com>
2025-08-03 02:50:08 +0800

    update

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -20,7 +20,6 @@
                      <span class="sr-only">Toggle sidebar</span>
                  </button>
                  <a href="https://flowbite.com" class="flex items-center justify-between mr-4">
-                     <img src="https://flowbite.s3.amazonaws.com/logo.svg" class="mr-3 h-8" alt="Flowbite Logo" />
                      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">GKS Reda
                          Pada</span>
                  </a>
